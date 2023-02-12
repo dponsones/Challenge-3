cc4594e4ce4e71ab96c8c281bae81b1c2ca01800
Deion Ponsones <dcponson@gmail.com>
2023-02-12 14:53:05 -0500

    added screenshot / comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// variables
 var passwordLength = 10; //random number reassigned at prompts
 var passwordArray = [];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
